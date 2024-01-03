c3a915b372e7b2984655339f6d157d12bb24f793
tijesus <emmanueltijesu@gmail.com>
2024-01-04 00:06:53 +0100

    alternating the alphabet

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,4 +3,3 @@ for i in reversed(range(65, 91)):
     """ assigning 32 to lower if the condition is met"""
     lower = 32 if (i % 2 == 0) else 0
     print("{}".format(chr(i + lower)), end="")
-    
