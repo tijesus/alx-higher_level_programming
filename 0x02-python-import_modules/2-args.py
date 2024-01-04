4521cd194ee7ca51edd67d4083a2dd1a43b41beb
tijesus <emmanueltijesu@gmail.com>
2024-01-04 15:41:26 +0100

    counting and printing arguments passed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,14 +2,14 @@
 import sys
 
 if __name__ == '__main__':
-    argv = sys.argv
-    argc = len(argv) - 1
 
-    if argc == 0:
+    count = len(sys.argv) - 1
+
+    if count == 0:
         print("0 argument")
-    elif argc == 1:
+    elif count == 1:
         print("1 arguement:")
     else:
-        print("{} arguments".format(argc))
-    for i in range(argc):
-        print("{}: {}".format(i + 1, argv[i + 1]))
+        print("{} arguments".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
