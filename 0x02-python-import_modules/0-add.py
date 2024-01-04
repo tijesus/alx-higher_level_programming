f3491aa7f136640a725e1798af3c8ab15f3dbbcb
tijesus <emmanueltijesu@gmail.com>
2024-01-04 12:38:04 +0100

    import the add module and must mot execute on__import__

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,5 +4,4 @@ import add_0
 if __name__ == "__main__":
     a = 1
     b = 2
-    sum = add_0.add(1, 2)
-    print("{:d} + {:d} = {:d}".format(a,b, sum))
+    print("{:d} + {:d} = {:d}".format(a,b, add_0.add(a, b)))
