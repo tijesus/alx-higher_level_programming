50097b75de6efce4d91934e7f06770c177400133
tijesus <emmanueltijesu@gmail.com>
2024-01-07 23:56:06 +0100

    return largest number in alist

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,11 +2,10 @@
 def max_integer(my_list=[]):
     if my_list == "":
         return None
-    else:
-        i = 0
-        while i < len(my_list):
-            if my_list[i] > my_list[i + 1]:
-                return my_list[i]
-            else:
-                return my_list[i + 1]
-        i += 1
+    max = my_list[0]
+    i = 0
+    while i < len(my_list):
+        if my_list[i] > max:
+            max = my_list[i]
+    i += 1
+    return max
