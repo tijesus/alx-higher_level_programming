ce7a2303c6399a659d1a8413f2f247764785c9ef
tijesus <emmanueltijesu@gmail.com>
2024-01-07 22:49:38 +0100

    add two tuples

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    #  for row in matrix:
-    #     end = ""
-    #     for column in row:
-    #         print("{}{:d}".format(end, column), end="")
-    #         end = " "
-    #     print("")
-
     for i in matrix:
         space = ""
         for j in i:
