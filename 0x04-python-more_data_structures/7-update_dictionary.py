d9120ba8f24e7e9add9858b19e41783822dbd7d1
tijesus <emmanueltijesu@gmail.com>
2024-01-10 22:36:09 +0100

    change the value of an element

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -3,5 +3,8 @@ def update_dictionary(a_dictionary, key, value):
     for _key in sorted(a_dictionary.keys()):
         if _key == key:
             a_dictionary[_key] = value
-        return (a_dictionary)
+            return (a_dictionary)
+    a_dictionary[key] = value
+    return a_dictionary
+
 
