fac11bcfbdf10e133860b0454645ea4f9e64cc7c
tijesus <emmanueltijesu@gmail.com>
2024-01-10 22:31:37 +0100

    change the value of an element

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    for _key in sorted(a_dictionary.keys()):
+        if _key == key:
+            a_dictionary[_key] = value
+        return (a_dictionary)
+
