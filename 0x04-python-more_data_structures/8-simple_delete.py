1dda5ee8d4502113c219193692e90847fa99902d
tijesus <emmanueltijesu@gmail.com>
2024-01-10 22:52:39 +0100

    delete key

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
+    for _key in sorted(a_dictionary.keys()):
+        if _key == key:
+            del(a_dictionary[_key])
+            return (a_dictionary)
     del(a_dictionary[key])
-    return (a_dictionary)
+    return a_dictionary
