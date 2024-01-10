a6f21fe7b3fa30f14f30c6cd7590d8a99a855c88
tijesus <emmanueltijesu@gmail.com>
2024-01-10 23:05:55 +0100

    delete an element in dict

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    for _key in sorted(a_dictionary.keys()):
-        if _key == key:
-            del(a_dictionary[_key])
-            return (a_dictionary)
-    del(a_dictionary[key])
-    return a_dictionary
+        if key in a_dictionary:
+            del (a_dictionary[key])
+        return a_dictionary
