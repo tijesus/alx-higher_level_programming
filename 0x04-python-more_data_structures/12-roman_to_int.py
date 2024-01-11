e09d784a1cf6598ec22152a216f5935766345ca2
tijesus <emmanueltijesu@gmail.com>
2024-01-11 01:43:05 +0100

    roman figure

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,24 +1,10 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string or type(roman_string) != str:
+    if not isinstance(roman_string, str) or not roman_string:
         return 0
     sum = 0
-    roman = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
-    for roman in reversed(roman_string):
-        numeral = roman[roman]
-        sum += numeral 
-        if sum < numeral * 5:
-            sum += numeral
-        else:
-            sum -= numeral
-    return sum
-
-#!/usr/bin/python3
-def roman_to_int(roman_string):
-    if not roman_string or type(roman_string) != str:
-        return 0
-    sum = 0
-    roman_numerals = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    roman_numerals = {'M': 1000, 'D': 500, 'C': 100,
+                      'L': 50, 'X': 10, 'V': 5, 'I': 1}
     prev = 0
 
     for i in reversed(roman_string):
