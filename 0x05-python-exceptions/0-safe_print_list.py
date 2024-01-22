3ca2790014f27e3be017bf3ea011db336925ed8d
tijesus <emmanueltijesu@gmail.com>
2024-01-22 17:36:51 +0100

    count list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,13 +2,11 @@
 def safe_print_list(my_list=[], x=0):
     count = 0
     try:
-        for i in my_list:
-            print(i, end="")
+        for i in range(x):
+            print(my_list[i], end="")
             count += 1
-            if count == x:
-                break
-        print()
+
     except IndexError:
         pass
-
+    print()
     return (count)
