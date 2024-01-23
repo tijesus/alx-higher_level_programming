b16162740606faea0f1b3ba48f54f1e0b8d6740c
tijesus <emmanueltijesu@gmail.com>
2024-01-23 17:23:51 +0100

    class of square using getter and setter

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -22,7 +22,7 @@ class Square:
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an inter")
-        elif value < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
