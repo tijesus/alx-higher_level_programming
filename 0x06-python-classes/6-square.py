5d02cd56cfd671f478ab1e13ab5bfb69c4523cfa
tijesus <emmanueltijesu@gmail.com>
2024-01-24 00:50:04 +0100

    print square with a position

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""class for a square"""
+
+
+class Square:
+    """class to handle square"""
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple) or len(value) != 2 or\
+                not all(isinstance(i, int) for i in value) or\
+                value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        return self.size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+            return
+        for i in range(self.position[1]):
+            print()
+        for i in range(self.size):
+            print("_" * self.position[0] + "#" * self.size)
