7820c2b3b566a76f54bc6aa046856357c3f25347
tijesus <emmanueltijesu@gmail.com>
2024-02-05 23:14:04 +0100

    function that checks the subclass of an objectclear

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,7 +4,7 @@
 
 def inherits_from(obj, a_class):
     """can also return true on subclass of an object"""
-    if not issubclass(obj, a_class):
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
         return True
     else:
         return False
