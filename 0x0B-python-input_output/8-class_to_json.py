6dbdcdca57e1d347d82bc531740f64f7608d4784
tijesus <emmanueltijesu@gmail.com>
2024-02-07 00:40:38 +0100

    class to json

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""function that returns dictionary discreption with 
+"""function that returns dictionary discreption with
     siple data sructure
 """
 
 
 def class_to_json(obj):
     """returns dictionary description of JSON
-        you can just return obj.__dict__    
+        you can just return obj.__dict__
     """
     new_json = {}
-    #__dict__.item() is a built in method to check the items of a dictionary
+    # __dict__.item() is a built in method to check the items of a dictionary
     for name, value in obj.__dict__.items():
         if isinstance(value, (list, str, bool, int, dict)):
             new_json[name] = value
