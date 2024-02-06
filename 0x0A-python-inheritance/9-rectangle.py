7ff6ad57a22d8d266b82301041f4816e7763bf39
tijesus <emmanueltijesu@gmail.com>
2024-02-06 01:52:39 +0100

    return area of a rectangle

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-"""class that raises error when empty"""
+"""Define Rectangle class"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """this inherits from base geometry"""
+    """Represent a object Rectangle"""
 
     def __init__(self, width, height):
-        """function to initialise the inheritancey"""
-        # super().__init__(width, height)s
+        """initialse the inheritance"""
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
 
     def area(self):
-        """funcion to return area"""
+        """Calculate the Rectagle's area"""
         return self.__width * self.__height
 
     def __str__(self):
-        """function to print"""
-        return "[Rectangele] {}/{}".format(self.__width, self.__height)
+        """Return description of the rectangle."""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
