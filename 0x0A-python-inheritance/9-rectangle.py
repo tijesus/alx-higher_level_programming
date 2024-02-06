f5915b1888d308c5a8b23681cf45c9991dffcc7c
tijesus <emmanueltijesu@gmail.com>
2024-02-06 01:50:14 +0100

    return area of a rectangle

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -9,9 +9,9 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         """function to initialise the inheritancey"""
         # super().__init__(width, height)s
-        super().integer_validator("width", width)
+        self.integer_validator("width", width)
         self.__width = width
-        super().integer_validator("height", height)
+        self.integer_validator("height", height)
         self.__height = height
 
     def area(self):
