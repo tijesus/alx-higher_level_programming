3800be485b3754d4134c1f55b7c00715a31b88ef
tijesus <emmanueltijesu@gmail.com>
2024-02-06 15:06:51 +0100

    read and print a fle

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""Function that reads test files and print 
+"""Function that reads test files and print
     to stdout
 """
+
 
 def read_file(filename=""):
     """Reads test files and prints"""
     with open(filename) as f:
         for line in f:
             print(line, end="")
-
