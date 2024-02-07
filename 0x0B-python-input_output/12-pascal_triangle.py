ec39418b94a5ca052ddd225fecb55729a3be4d7e
tijesus <emmanueltijesu@gmail.com>
2024-02-07 02:11:57 +0100

    pascal triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -5,11 +5,11 @@
 def pascal_triangle(n):
     """R Pascal's Triangle of size n"""
     if n <= 0:
-        return list()
-    triangle = list()
+        return []
+    triangle = []
     triangle.append([1])
     for i in range(1, n):
-        row = list()
+        row = []
         row.append(1)
         for j in range(1, i):
             row.append(triangle[i-1][j-1] + triangle[i-1][j])
