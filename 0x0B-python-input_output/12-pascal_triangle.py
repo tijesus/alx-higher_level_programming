4d1986f70cca6018ca752eee63ee6c64d3614d41
tijesus <emmanueltijesu@gmail.com>
2024-02-07 02:02:57 +0100

    pascal triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Function to return Pascal's Triangle"""
+
+
+def pascal_triangle(n):
+    """R Pascal's Triangle of size n"""
+    if n <= 0:
+        return []
+
+    triangles = [[1]]
+    while len(triangles) != n:
+        tri = triangles[-1]
+        tmp = [1]
+        for i in range(len(tri) - 1):
+            tmp.append(tri[i] + tri[i + 1])
+        tmp.append(1)
+        triangles.append(tmp)
+    return triangles
