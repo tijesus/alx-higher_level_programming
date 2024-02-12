ce5eef1612846c5ab149cd1c67d8149ed22a0d94
tijesus <emmanueltijesu@gmail.com>
2024-02-13 00:05:26 +0100

    fixed code requirement

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""This module contains a rectangle class"""
+"""Rectangle class"""
 
 from models.base import Base
 
 
 class Rectangle(Base):
-    """Represents a rectangle """
+    """Rectangle class containing its functions"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initializes attributes of the object"""
