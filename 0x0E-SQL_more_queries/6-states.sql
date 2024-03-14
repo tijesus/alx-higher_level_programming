38e1e540b1a12c05481477114c674f52ad903de7
tijesus <emmanueltijesu@gmail.com>
2024-03-14 11:08:28 +0100

    create database and table in the database with auto generation and primary key

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- create database and table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+    id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
