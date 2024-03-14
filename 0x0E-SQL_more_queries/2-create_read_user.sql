e92fdfb9df02ee5dba5add3a75adc9ae724b8dba
tijesus <emmanueltijesu@gmail.com>
2024-03-14 10:16:02 +0100

    create database with new user and grant select priviledge

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,4 +4,4 @@
 -- PRIVILEGES: CREATE, ALTER, DROP, INSERT, UPDATE, DELETE, SELECT, REFERENCES, RELOAD on *.* TO 'sammy'@'localhost' WITH GRANT OPTION;
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY "user_0d_2_pwd";
-GRANT ALL PRIVILEGES ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
