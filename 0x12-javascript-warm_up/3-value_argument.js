0794cfdc77644a9802a0e755a04a65949288a337
tijesus <emmanueltijesu@gmail.com>
2024-04-08 18:18:26 +0100

    print out argument value passed in the command line

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,9 +3,8 @@
 // console.log(process.argv[2] ? process.argv[2] : 'No argument');
 
 const argv = process.argv[2];
-if (argv  === undefined) {
+if (argv === undefined) {
   console.log('No argument');
-}
-else {
+} else {
   console.log(argv);
 }
