8d1db67c0828a6f2feffe0984f847d6f017a8379
tijesus <emmanueltijesu@gmail.com>
2024-04-15 21:36:41 +0100

    list out the states in the database

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """
+    Getting access to the database and get the states
+    """
+    db_connect = MySQLdb.connect(
+        host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
+
+    db_cursor = db_connect.cursor()
+
+    db_cursor.execute("SSELECT * FROM states ORDER BY id ASC")
+
+    rows_selected = db_cursor.fetchall()
+
+    for row in rows_selected:
+        print(row)
