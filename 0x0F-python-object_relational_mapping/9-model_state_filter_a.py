e4d03fc17ed4db71a83037ea9215ddb5f1ce6866
tijesus <emmanueltijesu@gmail.com>
2024-04-16 04:00:32 +0100

    more task

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This script that lists all states
+that contains lettter 'a' `hbtn_0e_6_usa`.
+"""
+
+from sys import argv
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    """Cheacking the database"""
+    db_path = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3])
+
+    engine = create_engine(db_path)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    states = session.query(State).filter(State.name.contains(a))
+    if states is not None:
+        for state in states:
+            print('{0}: {1}'.format(state.id, state.name))
