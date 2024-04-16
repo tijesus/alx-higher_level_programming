895ed4d4158302fe06fa205f7a68c6c568fa4784
tijesus <emmanueltijesu@gmail.com>
2024-04-16 04:32:35 +0100

    more task

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-This script updates the State object
-with id = 2 to the database `hbtn_0e_6_usa`.
+This script changes the name of a State object
+from the database `hbtn_0e_6_usa`.
 """
 
 from sys import argv
@@ -11,19 +11,20 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     """
-    update the database to get state object
+    Updates a State object on the database.
     """
 
-    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+    db_path = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
         argv[1], argv[2], argv[3])
 
-    engine = create_engine(db_url)
+    engine = create_engine(db_path)
     Session = sessionmaker(bind=engine)
 
     session = Session()
 
     state = session.query(State).filter(State.id == 2).first()
-    session.name = "New Mexico"
+    state.name = "New Mexico"
     session.commit()
 
     session.close()
+    
