2bcb448c415f8f7e8327029c5f185f3725e09654
tijesus <emmanueltijesu@gmail.com>
2024-04-16 05:04:37 +0100

    more task

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,27 +1,31 @@
 #!/usr/bin/python3
 """
-This script that lists all cities
-from the database `hbtn_0e_6_usa`.
+This script prints all City objects
+from the database `hbtn_0e_14_usa`.
 """
 
 from sys import argv
 from model_state import State, Base
-from model_city import State, City
+from model_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    """Cheacking the database"""
-    db_path = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+    """
+    list out the cities in databases
+    """
+
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
         argv[1], argv[2], argv[3])
 
-    engine = create_engine(db_path)
+    engine = create_engine(db_url)
     Session = sessionmaker(bind=engine)
 
     session = Session()
 
-    states_cities = session.query(City, State).join(State).all()
-    for city, state in states_cities:
+    state_cities = session.query(City, State).join(State)
+
+    for city, state in state_cities.all():
         print("{}: ({}) {}".format(state.name, city.id, city.name))
 
     session.commit()
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -2,15 +2,14 @@
 """
  Class State defination
 """
+from model_state import Base, State
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-from model_state import Base
 
 
 class City(Base):
     """The creation for the class object for cities"""
+    __tablename__ = 'cities'
 
-    __tablename__ = "cities"
-    id = Column(Integer, primary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('state_id'), nullable=False)
