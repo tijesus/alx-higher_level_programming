fb62b320306f2d5ea5cc5bafa2fffb8fb3e76b0a
tijesus <emmanueltijesu@gmail.com>
2024-04-16 04:28:36 +0100

    more task

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+This script updates the State object
+with id = 2 to the database `hbtn_0e_6_usa`.
+"""
+
+from sys import argv
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    """
+    update the database to get state
+    """
+
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3])
+
+    engine = create_engine(db_url)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    state = session.query(State).filter(State.id == 2).first()
+
+    new_state = State(name="Louisiana")
+    session.name ="New Mexico"
+    session.commit()
+
+    session.close()
