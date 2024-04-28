4da3ac3ef374680b1916347d7c65b329da62001f
tijesus <emmanueltijesu@gmail.com>
2024-04-28 23:49:43 +0100

    fetching from alx intranet

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,8 +4,8 @@
 import urllib.request
 
 if __name__ == '__main__':
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        content = response.read()
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
+        content = resp.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
