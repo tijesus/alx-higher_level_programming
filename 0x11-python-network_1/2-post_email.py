1ba0b30682e6940d2e5a9f48e0f4f28b1377f509
tijesus <emmanueltijesu@gmail.com>
2024-04-29 00:45:37 +0100

    post email to a url

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""post emailreceived from terminal"""
+
+import urllib.parse
+import urllib.request
+import sys
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    value = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode('ascii')
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as resp:
+        content = resp.read()
+    print("Your email is: {}".format(content.decode("utf-8")))
