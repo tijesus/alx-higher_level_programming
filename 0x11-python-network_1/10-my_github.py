bcc2b3ed235436a768790fb090f515fe8bb89307
tijesus <emmanueltijesu@gmail.com>
2024-04-30 00:14:38 +0100

    github credentials request

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Display the user GitHub id"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+
+    url = 'https://api.github.com/user'
+    req = requests.get(url, auth=(username, password))
+
+    req_id = r.json().get('id')
+    print(req_id)
