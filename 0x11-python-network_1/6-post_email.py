8c9f29e0a632153ec3edc25fd12905de163cfec5
tijesus <emmanueltijesu@gmail.com>
2024-04-29 01:48:13 +0100

    posting email to a url

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -4,8 +4,9 @@
 import requests
 import sys
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     url = sys.argv[1]
-    value = {'email': sys.argv[2]}
-    request = requests.post(url, data=value)
-    print(request.text)
+    value = {"email": sys.argv[2]}
+
+    req = requests.post(url, data=value)
+    print(req.text)
